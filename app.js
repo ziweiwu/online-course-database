faa643dff5217a3e47a9c01eca06556cd1a1bdae
ziwei wu <ziweiwu@gmail.com>
2018-03-06 17:11:00 -0500

    update

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mysql = require('mysql');
 
+// routes
 var index = require('./routes/index');
 var users = require('./routes/users');
 
